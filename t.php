0cca00349b62add029fc3be3fc5e3f468f2496a5
kotross <kotross@users.noreply.github.com>
2024-09-25 17:38:50 +0300

    Create t.php

---
+++ t.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Создай сам ))
+Там такой текст был
+[default]
+access_key = 184bed11b9ce0e73f219
+secret_key = XTxA0WHOKODkiDiu1DImx4IpOZkxnTA5cTODbxgg
+host_base = s3-msk.tinkoff.ru
+host_bucket = %(bucket)s.s3-msk.tinkoff.ru
+*/
